e91ed95108f29b139fa25c737f53e5e2b124a708
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-08-17 19:12:00 +0300

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -14,45 +14,46 @@ public class BubbleSort {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Dizinin eleman Sayısı:");
-		int total = sc.nextInt();
+		int size = sc.nextInt();
 		
-		int arr[] = new int[total];
+		int arr[] = new int[size];
 		
-		for(int i = 0; i < total; i++)
+		for(int i = 0; i < size; i++)
 		{
 			System.out.print("\n" + (i+1) + ".sayı = ");
 			arr[i] = sc.nextInt();
 		}
 		
 		System.out.print("\nSayıların Sıralanmamış Hali:");
-		for(int i = 0; i < total; i++)
+		for(int i = 0; i < size; i++)
 		{
 			System.out.print(" " + arr[i]);
 		}
 		
-		bubbleSort(arr, total);
+		bubbleSort(arr, size);
 		
 		System.out.print("\nSayıların Sıralanmış Hali");
-		for(int i = 0; i < total; i++)
+		for(int i = 0; i < size; i++)
 		{
 			System.out.print(" " + arr[i]);
 		}
 	}
 	
-	public static void bubbleSort (int arr[], int total){
+	public static void bubbleSort (int arr[], int size){
 		
 		int temp;
 		
-		for (int i = 1; i < total; i++)
+		for (int i = 1; i < size; i++)
 		{
-			for (int j = 0; j < (total - 1); j++)
+			for (int j = 0; j < (size - 1); j++)
 			{
 				if (arr[j] >= arr[j+1])
 				{
-			           temp = arr[j];
-	                   	   arr[j] = arr[j+1];
-	                           arr[j+1] = temp;
+					temp = arr[j];
+	                   		arr[j] = arr[j+1];
+	                                arr[j+1] = temp;
 	                        }
+			
 			}
 		}
 	}
