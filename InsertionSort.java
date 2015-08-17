3bbdb2fbd357af76e6a17947f2c651ad71e0b104
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-08-17 19:39:25 +0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -7,44 +7,42 @@ import java.util.Scanner;
  * @author handan 
  *
  */
-public class InsertionSort {
+public class InsertionSort 
+{
 	
-	public static void main (String[] args) throws IOException {
+	public static void main (String[] args) throws IOException 
+	{
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Dizinin eleman sayısı:");
 		int size = sc.nextInt();
 		int arr[] = new int[size];
 		
-		for (int i = 0; i < size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			System.out.print("\n" + (i+1) + ".sayıyı giriniz: ");
 			arr[i] = sc.nextInt();
 		}
 		System.out.print("\nDizinin Sıralanmamış Hali:");
-		for (int i = 0; i < size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			System.out.print(" " + arr[i]);
 		}
 		
-		insertionSort(arr, size);
+		insertionSort (arr, size);
 		
 		System.out.print("\nDizinin Sıralanmış Hali:");
-		for (int i = 0; i < size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			System.out.print(" " + arr[i]);
 		}
 	}
 	
-	public static void insertionSort (int arr[], int size) {
+	public static void insertionSort (int arr[], int size) 
+	{
 		
-		for (int i = 1; i < size; i++)
-		{
+		for (int i = 1; i < size; i++) {
 			int temp = arr[i];
 			int j = i;
 			
-			while (j > 0 && temp < arr[j-1])
-			{
+			while (j > 0 && temp < arr[j-1]) {
 				arr[j] = arr[j-1];
 				j--;
 			}
