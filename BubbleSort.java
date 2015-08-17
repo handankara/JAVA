8803e5635ecc123c354a2ff048a5cfade65609c0
Handan Kara <handan.kara@bil.omu.edu.tr>
2015-08-17 15:39:53 +0300

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,60 @@
+package com.algorithm;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author handan
+ *
+ */
+public class BubbleSort {
+	
+	public static void main (String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Dizinin eleman Sayısı:");
+		int total = sc.nextInt();
+		
+		int arr[] = new int[total];
+		
+		for(int i = 0; i < total; i++)
+		{
+			System.out.print("\n" + (i+1) + ".sayı = ");
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.print("\nSayıların Sıralanmamış Hali:");
+		for(int i = 0; i < total; i++)
+		{
+			System.out.print(" " + arr[i]);
+		}
+		
+		bubbleSort(arr, total);
+		
+		System.out.print("\nSayıların Sıralanmış Hali");
+		for(int i = 0; i < total; i++)
+		{
+			System.out.print(" " + arr[i]);
+		}
+	}
+	
+	public static void bubbleSort (int arr[], int total){
+		
+		int temp;
+		
+		for (int i = 1; i < total; i++)
+		{
+			for (int j = 0; j < (total - 1); j++)
+			{
+				if (arr[j] >= arr[j+1])
+				{
+					   temp = arr[j];
+	                   arr[j] = arr[j+1];
+	                   arr[j+1] = temp;
+	            }
+			
+			}
+		}
+	}
+}
