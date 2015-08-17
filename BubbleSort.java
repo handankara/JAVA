229240221b5ff4f9c36907a1726c9718a73e210a
Handan Kara <handan.kara@bil.omu.edu.tr>
2015-08-17 15:46:44 +0300

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -49,11 +49,10 @@ public class BubbleSort {
 			{
 				if (arr[j] >= arr[j+1])
 				{
-					   temp = arr[j];
-	                   arr[j] = arr[j+1];
-	                   arr[j+1] = temp;
-	            }
-			
+			           temp = arr[j];
+	                   	   arr[j] = arr[j+1];
+	                           arr[j+1] = temp;
+	                        }
 			}
 		}
 	}
