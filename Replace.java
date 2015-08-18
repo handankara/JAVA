402791b0298fbf8a09d8716306e9554d887bd81a
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-08-18 22:08:04 +0300

    Create Replace.java

---
+++ Replace.java
@@ -0,0 +1,35 @@
+package com.algorithm;
+
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * @author Handan Kara
+ *
+ * 
+ */
+public class deneme 
+{
+	public static void main (String[] args) throws IOException
+	{
+		Scanner sc = new Scanner (System.in); //Klavyeden veri almak için bu sınıf kullanılır.
+		System.out.print("Lütfen bir cümle giriniz:");
+		String str = sc.nextLine();
+		
+		replace (str);
+	}
+		
+	
+	public static void replace (String str) 
+	{
+		 char[] chars = str.toCharArray ();
+		 int size = chars.length;
+		 
+		 for (int i = 0; i < size; i++) {
+			 
+			 if (chars[i] == 'ü') {
+				 chars[i] = 'u';
+			 }
+		 }
+		 System.out.print (chars);
+	}
+}
