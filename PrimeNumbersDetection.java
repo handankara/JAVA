2048814a91f502d14b4603577abc3685173ee01b
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-08-19 12:27:59 +0300

    Update PrimeNumbersDetection.java

--- PrimeNumbersDetection.java
+++ PrimeNumbersDetection.java
@@ -1,7 +1,6 @@
 package com.javacodes;
 
 import java.util.Scanner;
-
 /*
  * @author Handan KARA
  * 
@@ -9,28 +8,28 @@ import java.util.Scanner;
 public class PrimeNumbersDetection 
 {
 	public static void main(String[] args)
-    {
+        {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print ("Lütfen Bir Sayı Giriniz:");
-	    int num = sc.nextInt ();
-	    primeNumberDetection(num);
-    }
+	        int num = sc.nextInt ();
+	        primeNumberDetection(num);
+        }
 	
 	public static void primeNumberDetection (int num)
 	{
 		int i, counter;
-		System.out.println("\n 1 'den girilen sayıya kadar olan asal sayıları bolup yazan program.\n");
+		System.out.println("\n 1 'den girilen sayıya kadar olan asal sayıları bulup yazan program.\n");
 		
 		for (counter = 2; counter < num; counter++) {
 			
 			for (i= 2; i < counter; i++) {
 				if ((counter % i) == 0) 
 					break;
-		    }
+		        }
 		
-		if (i == counter) 
-			System.out.println (counter + " sayısı asaldır");
+		        if (i == counter) 
+			    System.out.println (counter + " sayısı asaldır");
 		}
-    }
+        }
 }
